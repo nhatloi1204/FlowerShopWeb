687825f36fb1e6b15ca23a0c7764fd4485bd5078
Nhat Loi <nhatloi120402@gmail.com>
2026-05-18 02:00:36 +0700

    feat: update authentication role handling and cookie management

--- src/validations/auth.schema.ts
+++ src/validations/auth.schema.ts
@@ -29,10 +29,8 @@ export type LoginResponse = z.infer<typeof LoginResponseSchema>
 
 export interface JwtPayload {
   id: string
-  'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'?: string
-  'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'?:
-    | string
-    | string[]
+  email: string
+  role: string
   permissions: string[]
 }
 
